5d1f36bca6162d36e7e8b6e2da799f4ac3f809e2
Trần Xuân Bách <bach.tranxuan@hcmut.edu.vn>
2022-10-25 18:26:43 +0000

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 import Navbar from "./componets/Navbar"
 import Homepage from './pages/Homepage';
